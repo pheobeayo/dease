aad7bbf69222916b2a6876c513ca6b1e7b6b506a
pheobeayo <>
2025-01-19 21:44:18 +0100

    update the dashboard

--- src/pages/dashboard/ManageToken.jsx
+++ src/pages/dashboard/ManageToken.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import TokenInfoFetcher from '../../components/TokenInfoFetcher';
 
 
@@ -18,6 +18,13 @@ const ManageToken = () => {
 
   return (
     <main className="bg-[#02071D] font-dmsans">
+      <h1 className="text-white lg:text-[36px] md:text-[36px] text-[26px] font-[700] my-8 px-2">
+          Manage Token
+        </h1>
+        <h2 className="text-white lg:text-[20px] md:text-[20px] text-[15px] font-[500] my-8 px-2">
+        You currently do not have a token deployed yet, deploy a token
+        </h2>
+
      <TokenInfoFetcher />
     </main>
   )
